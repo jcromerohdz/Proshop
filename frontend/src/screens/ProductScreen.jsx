35ba9dd1e070a165f414bf1107dc884367892e1e
jcromerohdz <jcromerohdz@gmail.com>
2022-05-13 22:09:17 -0700

    ProductScreen population phase 1

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 const ProductScreen = () => {
+  const {id} = useParams()
+
+  const product = products.find((p) => p._id === id )
+
   return (
-    <div>ProductScreen</div>
+    <>
+      <Link
+        className='btn btn-light my-3'
+        to='/'
+      >
+        Go Back
+      </Link>
+      <Row>
+        <Col
+          md={6}
+        >
+          <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col
+          md={3}
+        >
+        </Col>
+      </Row>
+    </>
   )
 }
 
