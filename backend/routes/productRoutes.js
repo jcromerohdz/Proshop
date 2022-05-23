6f60374a448ff36b7443a3c93629311e1eccb38b
jcromerohdz <jcromerohdz@gmail.com>
2022-05-23 16:29:21 -0700

    Fetching products from the Data Base

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+const router = express.Router()
+
+// @desc   Fetch all products
+// @GET    GET api/products
+// @access Public 
+router.get('/', asyncHandler(async(req, res) => {
+  const products = await Product.find({})
+
+  res.json(products)
+}))
+
+// @desc   Fetch single product 
+// @GET    GET api/products/:id
+// @access Public 
+router.get('/:id', asyncHandler(async(req, res) => {
+  let product = '' 
+  if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+  // Yes, it's a valid ObjectId, proceed with `findById` call.
+    product = await Product.findById(req.params.id) 
+  }
+
+    if(product) {
+      res.json(product)
+    }else {
+      console.log()
+      res.status(404).json({message: 'Product not found'})
+    }
+
+}))
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config()
 const app = express()
-import products from './data/products.js'
 
 connectDB()
 const PORT =  process.env.PORT || 5000
@@ -14,14 +15,8 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
 
  app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
