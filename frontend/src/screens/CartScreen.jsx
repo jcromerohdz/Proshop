b43798223ee0ba69f0ef6e97035629a07e8ba1a9
J. Christian Romero <jcromerohdz@gmail.com>
2022-06-03 20:38:18 -0700

    Remove items from cart component

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -3,7 +3,7 @@ import { Link, useParams, useNavigate, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = () => {
   const {id} = useParams()
@@ -27,6 +27,7 @@ const CartScreen = () => {
 
   const removeFromCartHandler = (id) => {
     console.log('remove')
+		dispatch(removeFromCart(id))
 
   }
 
